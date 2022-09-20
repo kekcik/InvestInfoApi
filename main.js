583224af16954d7fa1dae013387e1a06ddc660a5
Ivan Trofimov <richard1997dog@gmail.com>
2022-09-20 14:01:27 +0300

    добил ыыд

--- main.js
+++ main.js
@@ -11,6 +11,9 @@ const requestListener = function (req, res) {
       res.end(data);
     })
     
+  } else if (req.url.includes("well-known/acme-challenge/QlSkqeYzOCCFB-ANMYHxnJjt5okp1_87aGXu8mAQYo8")) {
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    res.end("QlSkqeYzOCCFB-ANMYHxnJjt5okp1_87aGXu8mAQYo8.J1C9f7bOoW-Ex3EZ3zlNfO1P6SIHBOR9l-XOXStLM20");
   } else if (req.url.includes("/rates/")) {
 
     fs.readFile("rates.json", function(err, data) {
@@ -32,4 +35,4 @@ const requestListener = function (req, res) {
 }
 
 const server = http.createServer(requestListener);
-server.listen(8080); 
+server.listen(80); 
